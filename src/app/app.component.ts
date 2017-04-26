47c45e4b3d67e6495843ab778897703a552ffdd0
padamaszek <ursus707@gmail.com>
2017-04-26 21:14:55 +0200

    CarPart List and 2way binding + simple page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,40 @@
 import { Component } from '@angular/core';
 
+export class CarPart {
+  id: number;
+  name: string;
+}
+const CARPARTS: CarPart[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{part.name}} details!</h2>
+  <div><label>id: </label>{{part.id}}</div>
+  <div>
+      <label>name: </label>
+      <input [(ngModel)]="part.name" placeholder="name">
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Car Configurator';
+	carParts = CARPARTS
+ 	part: CarPart = {
+  	id: 1,
+  	name: 'Engine'
+}; 
+}
+
