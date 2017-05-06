2d6663c6b36b9fe6f18087f845fdd0fcfde8ea13
padamaszek <ursus707@gmail.com>
2017-05-06 14:19:18 +0200

    styles and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-export class CarPart {
+export class Carpart {
   id: number;
   name: string;
 }
-const CARPARTS: CarPart[] = [
+const CARPARTS: Carpart[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -21,18 +21,74 @@ const CARPARTS: CarPart[] = [
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <h2>{{part.name}} details!</h2>
-  <div><label>id: </label>{{part.id}}</div>
+  <h2>My Carparts</h2>
+<ul class="carparts">
+  <li *ngFor="let carpart of carparts">
+   <span class="badge">{{carpart.id}}</span> {{carpart.name}}
+    </li>
+</ul>
+  <h2>{{carpart.name}} details!</h2>
+  <div><label>id: </label>{{carpart.id}}</div>
   <div>
       <label>name: </label>
-      <input [(ngModel)]="part.name" placeholder="name">
+      <input [(ngModel)]="carpart.name" placeholder="name">
     </div>
   `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .carparts {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .carparts li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .carparts li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .carparts li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .carparts .text {
+    position: relative;
+    top: -3px;
+  }
+  .carparts .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
 })
 export class AppComponent  { 
 	title = 'Car Configurator';
-	carParts = CARPARTS
- 	part: CarPart = {
+	carparts = CARPARTS;
+ 	carpart: CarPart = {
   	id: 1,
   	name: 'Engine'
 }; 
